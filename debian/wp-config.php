9d6922cdb8048f18a9e7b33bb0f6583cf4e525ad
Raphaël Hertzog <hertzog@debian.org>
2010-06-20 22:23:58 +0200

    Improve wp-config.php to support sites on subdomains and htaccess by providing directives ready to uncomment

--- debian/wp-config.php
+++ debian/wp-config.php
@@ -1,27 +1,49 @@
 <?php
-/** WordPress's Debianised default master config file
-Please do NOT edit and read about how the configuration works in the README.Debian
-**/
+/***
+ * WordPress's Debianised default master config file
+ * Please do NOT edit and learn how the configuration works in
+ * /usr/share/doc/wordpress/README.Debian
+ ***/
 
-    #http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=435289
-    #http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=440572 (underscores, not dashes)
-    $debian_server = preg_replace('/:.*/', "", $_SERVER['HTTP_HOST']);
-    $debian_server = preg_replace("/[^a-zA-Z0-9.\-]/", "", $debian_server);
-    $debian_file = '/etc/wordpress/config-'.strtolower($debian_server).'.php';
+/* Look up a host-specific config file in
+ * /etc/wordpress/config-<host>.php or /etc/wordpress/config-<domain>.php
+ */
+$debian_server = preg_replace('/:.*/', "", $_SERVER['HTTP_HOST']);
+$debian_server = preg_replace("/[^a-zA-Z0-9.\-]/", "", $debian_server);
+$debian_file = '/etc/wordpress/config-'.strtolower($debian_server).'.php';
+/* Main site in case of multisite with subdomains */
+$debian_main_server = preg_replace("/^[^.]*\./", "", $debian_server);
+$debian_main_file = '/etc/wordpress/config-'.strtolower($debian_main_server).'.php';
 
-    if (!file_exists($debian_file)) {
-        header("HTTP/1.0 404 Not Found");
-	echo "<b>$debian_file</b> could not be found. The file is either not readable by this process or does not exist. <br>
-		Please check if <b>$debian_file</b> exists and contains the right password/username.";
-        exit(1);
-    }
-
+if (file_exists($debian_file)) {
     require_once($debian_file);
+    define('DEBIAN_FILE', $debian_file);
+} elseif (file_exists($debian_main_file)) {
+    require_once($debian_main_file);
+    define('DEBIAN_FILE', $debian_main_file);
+} elseif (file_exists("/etc/wordpress/config-default.php")) {
+    require_once("/etc/wordpress/config-default.php");
+    define('DEBIAN_FILE', "/etc/wordpress/config-default.php");
+} else {
+    header("HTTP/1.0 404 Not Found");
+    echo "Neither <b>$debian_file</b> nor <b>$debian_main_file</b> could be found. <br/> Ensure one of them exists, is readable by the webserver and contains the right password/username.";
+    exit(1);
+}
 
+/* Default value for some constants if they have not yet been set
+   by the host-specific config files */
 define('ABSPATH', '/usr/share/wordpress/');
 define('WP_CORE_UPDATE', false);
 define('WP_ALLOW_MULTISITE', true);
-define('DEBIAN_FILE', $debian_file);
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_HOST', 'localhost');
 
-require_once(ABSPATH.'wp-settings.php');
+/* Default value for the table_prefix variable so that it doesn't need to
+   be put in every host-specific config file */
+if (!isset($table_prefix)) {
+    $table_prefix = 'wp_';
+}
+
+require_once(ABSPATH . 'wp-settings.php');
 ?>
