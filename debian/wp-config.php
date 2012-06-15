b6023722a21cec8f184125455c247f0874feac0f
Raphaël Hertzog <hertzog@debian.org>
2012-06-15 16:33:12 +0200

    Improve wp-config.php so that WordPress works behind an https reverse-proxy.
    If we didn't set this, WordPress would generate http links instead
    of https links because it has been contacted over http while the client
    contacted the reverse proxy over https.

--- debian/wp-config.php
+++ debian/wp-config.php
@@ -45,5 +45,8 @@ if (!isset($table_prefix)) {
     $table_prefix = 'wp_';
 }
 
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    $_SERVER['HTTPS'] = 'on';
+
 require_once(ABSPATH . 'wp-settings.php');
 ?>
